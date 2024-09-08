7063a6c357eb2c4e9f2b766daea8877d89d06808
Felix_SANA <55303494+systemannounce@users.noreply.github.com>
2024-09-08 15:42:48 +0800

    MainFunction

---
+++ steam.py
@@ -0,0 +1,161 @@
+import requests
+import json
+import pandas as pd
+from datetime import datetime
+from io import StringIO
+import os
+
+
+class SteamFriends:
+    def __init__(self):
+        self.steam_web_api = os.environ.get('web_api')
+        self.steam_id = os.environ.get('id')
+
+        self.friends = 0        # 总数
+        self.friend_ids = []    # 向steam请求的id列表（url）
+        self.friends_list = {}  # 查询到的所有好友，以及：好友日期
+
+        self.steamid = []       # 点击立即跳转到对应界面（Markdown格式）
+        self.steamid_num = []   # 记录一份只有id的列表
+        self.bfd = []           # 成为好友的日期
+        self.name = []          # steam资料名
+        self.profileurl = []    # 暂时没用
+        self.avatar = []        # 头像（Markdown格式）
+
+        self.friend_list_url = 'https://api.steampowered.com/ISteamUser/GetFriendList/v0001/'
+        self.friend_summaries_url = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/'
+        self.sess = requests.Session()
+        self.sess.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537'
+                                                '.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'})
+
+    def GetFriendList(self):
+        params = {
+            'key': self.steam_web_api,
+            'steamid': self.steam_id,
+        }
+        response = self.sess.get(self.friend_list_url, params=params)
+        json_list = json.loads(response.text)
+        self.friends_list = {friend['steamid']: friend['friend_since'] for friend in json_list['friendslist']['friends']}
+        self.friends = len(self.friends_list)
+
+    def GetFriendsSummaries(self):
+        for num, id in enumerate(self.friends_list):
+            self.friend_ids.append(id)
+            if (num + 1)%100 == 0:
+                self.GetFriendsStatus()
+                self.friend_ids = []
+        self.GetFriendsStatus()
+
+
+    def GetFriendsStatus(self):
+        if not self.friend_ids:
+            return False
+        steam_ids = ''
+        for id in self.friend_ids:
+            steam_ids = steam_ids + id + ','
+        steam_ids = steam_ids[:-1]
+        params = {
+            'key': self.steam_web_api,
+            'steamids': steam_ids,
+        }
+        response = self.sess.get(self.friend_summaries_url, params=params)
+        json_list = json.loads(response.text)
+        users_list = json_list['response']['players']
+        for user in users_list:
+            self.steamid_num.append(user['steamid'])
+            self.steamid.append('[' + user['steamid'] + '](https://steamcommunity.com/profiles/' + user['steamid'] + '/)')
+            self.name.append(user['personaname'])
+            self.avatar.append('![](' + user['avatar'] + ')')
+
+    def CreateFrom(self):
+        with open('./README.md', 'r') as file:
+            original_content = file.read()
+        is_friend = ['✅' for _ in self.avatar]
+        empty_list = ['' for _ in self.avatar]
+        for steamid in self.steamid_num:
+            bfd_unix = self.friends_list[steamid]
+            self.bfd.append(datetime.utcfromtimestamp(bfd_unix).strftime('%Y-%m-%d %H:%M:%S'))
+        data = {
+            'Avatar': self.avatar,
+            'Name': self.name,
+            'steamid': self.steamid,
+            'is_friend': is_friend,
+            'BFD': self.bfd,
+            'Remark': empty_list
+        }
+        df = pd.DataFrame(data)
+        markdown_table = df.to_markdown(index=False)
+        updated_content = f"{original_content}\n\n## Steam好友列表\n\n{markdown_table}"
+        with open('./README.md', 'w') as file:
+            file.write(updated_content)
+
+    def Update(self):
+        with open('./README.md', 'r') as file:
+            content = file.readlines()
+
+        # 找到 Markdown 表格的开始位置
+        table_start_index = None
+        for i, line in enumerate(content):
+            if line.strip().startswith('|'):
+                table_start_index = i
+                break
+
+        # 提取表格内容
+        table_content = ''.join(content[table_start_index:])
+
+        # 转换 Markdown 表格为 pandas DataFrame
+        # 去掉表头的分隔线
+        table_content = '\n'.join(line for line in table_content.strip().split('\n') if not line.startswith('|:'))
+
+        # 使用 tabulate 解析表格内容
+        try:
+            df = pd.read_csv(StringIO(table_content), sep='|', engine='python', skipinitialspace=True)
+            df.columns = [col.strip() for col in df.columns]  # 去掉列名的多余空格
+            df = df.apply(lambda x: x.map(lambda y: y.strip() if isinstance(y, str) else y))  # 去除每一个值里面多余的空格
+            df = df.iloc[:, 1:-1]  # 去掉第一列和最后一列的空白列
+            df = df.fillna('')  # 删除所有NaN单元格
+        except Exception as e:
+            print("Error:", e)
+            df = pd.DataFrame()  # 处理错误时返回空 DataFrame
+
+        # 重新判断好友
+        df['is_friend'] = '❌'
+        for num, id in enumerate(self.steamid):
+            if df[df['steamid'] == id].empty:
+                # print("没有找到匹配的 ID")
+                new_friend = {
+                    'Avatar': self.avatar[num],
+                    'Name': self.name[num],
+                    'steamid': self.steamid[num],
+                    'is_friend': '✅',
+                    'BFD': datetime.utcfromtimestamp(self.friends_list[self.steamid_num[num]]).strftime('%Y-%m-%d %H:%M:%S'),
+                    'Remark': ''
+                }
+                df.loc[len(df)] = new_friend
+            else:
+                df.loc[df['steamid'] == id, 'is_friend'] = '✅'
+                df.loc[df['steamid'] == id, 'Avatar'] = self.avatar[num]
+                df.loc[df['steamid'] == id, 'Name'] = self.name[num]
+        updated_markdown_table = df.to_markdown(index=False)
+        updated_content = ''.join(content[:table_start_index]) + updated_markdown_table
+        with open('./README.md', 'w') as file:
+            file.write(updated_content)
+
+    def UpdateOrCreate(self):
+        with open('./README.md', 'r') as file:
+            original_content = file.read()
+        if '|' in original_content:
+            self.Update()
+        else:
+            self.CreateFrom()
+
+    def GetData(self):
+        self.GetFriendList()
+        self.GetFriendsSummaries()
+        self.UpdateOrCreate()
+
+
+
+if __name__ == '__main__':
+    app = SteamFriends()
+    app.GetData()
